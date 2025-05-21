69290f896fb7025939f702499a8495b2098a666b
Emrah Şander <emrahsander@Emrah-MacBook-Air.local>
2025-05-21 07:21:29 +0300

    og image 3

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,7 +4,7 @@ export const metadata = {
   title: "Kaşarcım - Türkiye'nin En Lezzetli ve Organik Kaşar Peyniri",
   description: "Türkiye'nin en kaliteli süt ürünleri ve peynir çeşitlerini keşfedin. Doğal süt ve geleneksel yöntemlerle ürettiğimiz kaşar peynirlerimizle lezzeti sofranıza getiriyoruz.",
   openGraph: {
-    title: "KKaşarcım - Türkiye'nin En Lezzetli ve Organik Kaşar Peyniri",
+    title: "Kaşarcım - Türkiye'nin En Lezzetli ve Organik Kaşar Peyniri",
     description: "Doğal süt ve geleneksel yöntemlerle üretilen eşsiz tadıyla peynir çeşitleri. Sizin için özenle hazırlanıp gönderiliyor.",
     images: ['https://cdn.kasarcim.com/727b287e1370e7bc_logo.png'],
     type: 'website',
